2317366db4285a33d24ebb23f63aa5a867446ed8
hmtesser <hmtesser@gmail.com>
2022-08-29 01:08:07 -0300

    historico, autoquery, impressao, falta atualização da tela

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,11 @@
 import Home from './routes/Home.html';
 import About from './routes/About.html';
+import CharSelection from './components/characterSelection.html';
+import History from './components/history.html';
 
 export default {
   '/': Home,
   '/about': About,
+  '/charSelection': CharSelection,
+  '/history': History,
 };
